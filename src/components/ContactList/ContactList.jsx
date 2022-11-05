d2f9f3844b8f34de3a2761600ab709943e33ce07
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-11-05 15:50:49 +0200

    update ContactsList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,34 +1,24 @@
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contacts/contactsSlice';
+import { useFilteredContacts } from 'hooks/useFilteredContacts';
 import { Button } from 'components/Button/Button.styled';
 import { List, Item } from './ContactList.styled';
 
-export const ContactList = ({ contacts, onRemoveContact }) => {
+export const ContactList = () => {
+  const dispatch = useDispatch();
+
+  const filteredContacts = useFilteredContacts();
+
   return (
     <List>
-      {contacts.map(({ name, id, number }) => (
+      {filteredContacts.map(({ name, id, number }) => (
         <Item key={id}>
           <span>{name}:</span> {number}
-          <Button
-            type="button"
-            onClick={() => {
-              onRemoveContact(id);
-            }}
-          >
+          <Button type="button" onClick={() => dispatch(deleteContact(id))}>
             Delete
           </Button>
         </Item>
       ))}
     </List>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string,
-      name: PropTypes.string,
-      number: PropTypes.string,
-    })
-  ).isRequired,
-  onRemoveContact: PropTypes.func.isRequired,
 };
